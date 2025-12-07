0dcd79ed0364ad9de58dad7d81cd0d1d299a8475
rupesh <rupeshsaha899@gmail.com>
2025-12-07 11:23:15 +0530

    add shadcn components and init auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-import { getQueryClient, trpc } from "@/trpc/server"
-import { Client } from "./client"
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { authClient } from "@/lib/auth-client";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import React from "react";
 
-
-
-const page =async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+const page = async () => {
+  await requireAuth();
+  const data = await caller.getUsers()
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>loading...</p>}>
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(data,null,5)}
+      
+    
+    </div>
+  );
+};
 
-    <Client />
-      </Suspense>
-    </HydrationBoundary>
-  )
-}
-
-export default page
+export default page;
